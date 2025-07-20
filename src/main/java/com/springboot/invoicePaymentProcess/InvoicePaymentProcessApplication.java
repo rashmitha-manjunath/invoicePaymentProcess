86be1890dbee1b71e8f6f6caee208810bde01aae
Rashmitha Manjunath <rashmithagujran@gmail.com>
2025-07-20 17:24:25 +0530

    fixing bootrun

--- src/main/java/com/springboot/invoicePaymentProcess/InvoicePaymentProcessApplication.java
+++ src/main/java/com/springboot/invoicePaymentProcess/InvoicePaymentProcessApplication.java
@@ -2,8 +2,10 @@ package com.springboot.invoicePaymentProcess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.springboot.invoicePaymentProcess"})
 public class InvoicePaymentProcessApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/springboot/invoicePaymentProcess/repositories/InvoiceRepository.java
+++ src/main/java/com/springboot/invoicePaymentProcess/repositories/InvoiceRepository.java
@@ -3,9 +3,11 @@ package com.springboot.invoicePaymentProcess.repositories;
 import com.springboot.invoicePaymentProcess.model.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
     @Query("SELECT i FROM Invoice i WHERE i.dueDate < CURRENT_DATE AND i.status NOT IN ('PAID', 'VOID')")
     List<Invoice> findOverdueInvoices();
